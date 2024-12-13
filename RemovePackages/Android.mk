a77eb221398a8ca748b20d4d6efdb1da701eb9c6
Rve27 <rve27github@gmail.com>
2024-12-14 06:42:19 +0700

    beryllium: Don't remove Abstruct
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -11,8 +11,7 @@ LOCAL_OVERRIDES_PACKAGES := \
     Maps \
     YouTube \
     PersonalSafety \
-    GoogleCameraGo \
-    Abstruct
+    GoogleCameraGo
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
